30593121a014c89268ade009c84c3533ef2f8f7d
Michael-Bombini <bombini.michael@gmail.com>
2022-05-20 14:35:39 +0200

    Started working on milestone 2 and 3

--- js/main.js
+++ js/main.js
@@ -27,3 +27,25 @@ for(let i = 1 ; i <= 100 ; i++){
         console.log(i);
     }
 }
+
+
+const containerElement = document.querySelector(".row");
+
+
+
+for(let i = 1 ; i <= 100 ; i++){
+    if(i%3===0 && i%5===0){
+        console.log("FizzBuzz");
+    }
+    else if(i%5===0){
+        console.log("Buzz");
+    }
+    else if (i%3===0){
+        console.log("Fizz");
+    }
+    else{
+        console.log(i);
+    }
+    containerElement.innerHTML += `<div class="square"><span class="text-white"></span></div>`;
+
+}
